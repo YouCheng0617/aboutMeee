90e58778c76a88d91841507b92d6cfe9cc639830
yogurt0617 <20070617TimmyAlan@gmail.com>
2026-03-10 22:16:29 +0800

    新增友站(對不起我忘了一個人很久)

--- src/pages/friend.jsx
+++ src/pages/friend.jsx
@@ -12,6 +12,10 @@ export default function Friend(){
                    <a href="https://qwo877.github.io/me/home.html" target='_blank' id='Moew'>匿名用戶9487</a>
                    、
                    <a href="https://itousouta15.tw/" target='_blank' style={{ color: "#5d7da1" }}>伊藤蒼太</a>
+                   、
+                   <a href="https://chuen666666.github.io/" target='_blank' style={{color:"#6b61f8"}}>淳</a>
+                   、
+                   <a href="https://xxuanxiao.pages.dev/"  target='_blank' style={{color:"#fff000"}}>xuanxiao</a>
                  </p>
                </div>
     )
