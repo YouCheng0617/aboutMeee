95c1525abb5890c960aa24cf31350f269ea3fd30
yogurt0617 <20070617TimmyAlan@gmail.com>
2026-04-09 00:50:05 +0800

    you guess

--- src/pages/friend.jsx
+++ src/pages/friend.jsx
@@ -1,22 +1,25 @@
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import './friend.css'
 
-export default function Friend(){
-    
-    return(
-     <div className='friend'>
-                 <p>
-                   友達のWeb：
-                   <a href="https://yuzen.dev" target='_blank' style={{ color: "#DAA520" }}>Zn</a>
-                   、
-                   <a href="https://qwo877.github.io/me/home.html" target='_blank' id='Moew'>匿名用戶9487</a>
-                   、
-                   <a href="https://itousouta15.tw/" target='_blank' style={{ color: "#5d7da1" }}>伊藤蒼太</a>
-                   、
-                   <a href="https://chuen666666.github.io/" target='_blank' style={{color:"#6b61f8"}}>淳</a>
-                   、
-                   <a href="https://xxuanxiao.pages.dev/"  target='_blank' style={{color:"#fff000"}}>xuanxiao</a>
-                 </p>
-               </div>
-    )
+export default function Friend() {
+
+  return (
+    <div className='friend'>
+      <p>
+        友達のWeb：
+        <a href="https://yuzen.dev" target='_blank' style={{ color: "#DAA520" }}>Zn</a>
+        、
+        <a href="https://blog.siewilly.me/" target='_blank' style={{ color: "#7b00ff" }}>南宮有栖</a>
+        、
+        <a href="https://qwo877.github.io/me/home.html" target='_blank' id='Moew'>匿名用戶9487</a>
+        、
+        <a href="https://itousouta15.tw/" target='_blank' style={{ color: "#5d7da1" }}>伊藤蒼太</a>
+        、
+        <a href="https://chuen666666.github.io/" target='_blank' style={{ color: "#6b61f8" }}>淳</a>
+        、
+        <a href="https://xxuanxiao.pages.dev/" target='_blank' style={{ color: "#fff000" }}>xuanxiao</a>
+
+      </p>
+    </div>
+  )
 }
